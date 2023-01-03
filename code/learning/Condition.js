ad5f1cf5de026e1d1fc93f70b314a5374bccee92
SuBo <nhocbjke6666@gmail.com>
2023-01-03 18:17:47 +0700

    ex

---
+++ code/learning/Condition.js
@@ -0,0 +1,23 @@
+// let number = number(prompt('Nhập vào số bạn muốn kiểm tra: '))
+
+// if (number % 2 == 0){
+//     console.log('Đây là số chẵn')
+// }
+// else{
+//     console.log('Đây là số lẻ');
+// }
+
+let x = Number(prompt('Nhập một số x: '))
+
+if (x<=10 && x>=9)
+    console.log("Xuất sắc");
+else if (x<9 && x>=8)
+    console.log("Giỏi");
+    else if (x<8 && x>=7)
+        console.log("Khá");
+        else if (x<7 && x>=5)
+            console.log("Trung bình");
+            else if (x<5 && x>=0)
+                console.log("Yếu");
+                else console.log("Không hợp lệ");
+    
